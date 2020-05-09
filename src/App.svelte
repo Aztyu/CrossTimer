755227ddb0a41c348c8daf78901b68a3b4f9689a
Corentin BEAL <coco.bea@hotmail.fr>
2020-05-09 23:27:18 +0200

    Gestion d'un timer + ajout de bips

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,59 @@
 <script>
+	import Timer from './Timer.svelte';
+
 	export let counter;
 
 	let minutes = 0;
 	let seconds = 0;
 	let time = 0;
 
+	var beepLong = new Audio('sound/beeplong.mp3');
+	var beepCourt = new Audio('sound/beepcourt.mp3');
+
+	let timers = [
+		{ id: 1, time: 0 }
+	];
+
+	var timer = function() {
+		console.log('1sec passée');
+		time = time - 1;
+		if (time > 0) {
+			setTimeout(timer, 1000);
+			if (time < 4) {
+				beepCourt.play();
+			}
+		} else {
+			beepLong.play();
+		}
+	};
+
 	function startTimer() {
-		time = +minutes * 60 + +seconds;
+		console.log(timers);
+		time = timers[0].time;
+		setTimeout(timer, 1000);
+	}
+
+	function handleUpdate(event) {
+		const timeData = event.detail;
+		
+		const timer = timers.filter(obj => {
+			return obj.id === timeData.id
+		});
+		timer[0].time = timeData.time;
 	}
 </script>
 
 <main>
 	<h1>Timer</h1>
+	
+	{#each timers as time}
+		<Timer id={time.id} on:update={handleUpdate}/>
+	{/each}
+
 	<p>
-		<input bind:value={minutes}/>
-		<span>m </span>
-		<input bind:value={seconds}/>
-		<span>s </span>
 		<button on:click={startTimer}>Start</button>
 	</p>
-	
+
 	<p>
 		{#if time > 0 }
 			<span>Time left : {time}</span>
---
+++ src/Timer.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    export let id;
+
+    function sendUpdateTimer(time) {
+		dispatch('update', {
+            id: id,
+			time: time
+		});
+    }
+    
+    $: {
+        sendUpdateTimer(+minutes * 60 + +seconds);
+    }
+
+	let minutes = 0;
+	let seconds = 0;
+</script>
+
+<p>
+    <input bind:value={minutes}/>
+    <span>m </span>
+    <input bind:value={seconds}/>
+    <span>s </span>
+</p>
